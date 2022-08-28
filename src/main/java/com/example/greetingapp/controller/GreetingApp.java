b8f26d09370cc6098866c44e3ae5a9bff48b9838
santhoshkn011 <santhoshkn011@gmail.com>
2022-08-28 12:44:34 +0530

    Added: Created Greeting controller

---
+++ src/main/java/com/example/greetingapp/controller/GreetingApp.java
@@ -0,0 +1,25 @@
+package com.example.greetingapp.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/greet")
+public class GreetingApp {
+
+    @RequestMapping(value = {"","/","/home"}, method = RequestMethod.GET)
+    public String greet(){
+        return "Hello, this is Santhosh from BridgeLabz";
+    }
+
+    @RequestMapping("/query/{name}")
+    public String greet1(@PathVariable String name){
+        return "<h1><font color=blue>Hello! This is " + name + " From BridgeLabz!!</font></h1>" +
+                "</br> <font color=green>Passing name as a path variable.</font>";
+    }
+
+    @GetMapping("/query")
+    public String getUsers(@RequestParam(value = "name", defaultValue = "Santhosh") String name) {
+        return "<h1><font color=red>Hello! This is " + name + " From BridgeLabz!!</font></h1>" +
+                "</br> <font color=green>Passing name as a parameter.</font>";
+    }
+}
---
+++ src/test/java/com/example/greetingapp/GreetingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greetingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
