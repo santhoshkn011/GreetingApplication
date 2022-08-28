79cac0c57fc0c544f0815032cef3a891b22a3b87
santhoshkn011 <santhoshkn011@gmail.com>
2022-08-28 12:46:01 +0530

    Added: Greeting Controller

--- src/main/java/com/example/greetingapp/controller/GreetingApp.java
+++ src/main/java/com/example/greetingapp/controller/GreetingApp.java
@@ -16,7 +16,6 @@ public class GreetingApp {
         return "<h1><font color=blue>Hello! This is " + name + " From BridgeLabz!!</font></h1>" +
                 "</br> <font color=green>Passing name as a path variable.</font>";
     }
-
     @GetMapping("/query")
     public String getUsers(@RequestParam(value = "name", defaultValue = "Santhosh") String name) {
         return "<h1><font color=red>Hello! This is " + name + " From BridgeLabz!!</font></h1>" +
